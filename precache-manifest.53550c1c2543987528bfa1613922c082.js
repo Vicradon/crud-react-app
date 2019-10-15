c1fdc512d2e860063827c537aba47261d957fae5
Victor Rahael <vicraph7@gmail.com>
2019-10-15 01:29:10 +0100

    Updates

---
+++ precache-manifest.53550c1c2543987528bfa1613922c082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367bd752c98ee12abc12a3735eb7f18e",
+    "url": "/crud-react-app/index.html"
+  },
+  {
+    "revision": "55716144c94988c6d531",
+    "url": "/crud-react-app/static/css/main.774f3d50.chunk.css"
+  },
+  {
+    "revision": "cee46ffdbd9eb3727f40",
+    "url": "/crud-react-app/static/js/2.7f2ee637.chunk.js"
+  },
+  {
+    "revision": "55716144c94988c6d531",
+    "url": "/crud-react-app/static/js/main.93d28530.chunk.js"
+  },
+  {
+    "revision": "79af996d568035c281ba",
+    "url": "/crud-react-app/static/js/runtime-main.60f6e75b.js"
+  }
+]);
