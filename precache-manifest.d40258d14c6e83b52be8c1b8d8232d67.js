b8e7ddce04048c7c230f80f92ac578959ece7207
Victor Rahael <vicraph7@gmail.com>
2019-10-15 01:39:55 +0100

    Updates

---
+++ precache-manifest.d40258d14c6e83b52be8c1b8d8232d67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202d6647edd46965e46654fad1a5aedb",
+    "url": "/awesome-react-app/index.html"
+  },
+  {
+    "revision": "55716144c94988c6d531",
+    "url": "/awesome-react-app/static/css/main.774f3d50.chunk.css"
+  },
+  {
+    "revision": "cee46ffdbd9eb3727f40",
+    "url": "/awesome-react-app/static/js/2.7f2ee637.chunk.js"
+  },
+  {
+    "revision": "55716144c94988c6d531",
+    "url": "/awesome-react-app/static/js/main.93d28530.chunk.js"
+  },
+  {
+    "revision": "23fac4c7a885a7c56ced",
+    "url": "/awesome-react-app/static/js/runtime-main.ef1694af.js"
+  }
+]);
