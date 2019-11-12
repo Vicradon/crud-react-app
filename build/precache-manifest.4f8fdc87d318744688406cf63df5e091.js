ad0575ab623886eee7cbc7f19de680c7dfd67dac
Vicradon <vicraph7@gmail.com>
2019-11-12 14:05:28 +0100

    deploying

---
+++ build/precache-manifest.4f8fdc87d318744688406cf63df5e091.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca390a32f4428ef32796bdaa4d96a9c",
+    "url": "/crud-react-app/index.html"
+  },
+  {
+    "revision": "e8f79a95173fe868d205",
+    "url": "/crud-react-app/static/css/main.813e6cfb.chunk.css"
+  },
+  {
+    "revision": "fedf672ea3cc2ae171af",
+    "url": "/crud-react-app/static/js/2.b215fe25.chunk.js"
+  },
+  {
+    "revision": "e8f79a95173fe868d205",
+    "url": "/crud-react-app/static/js/main.8091f7fe.chunk.js"
+  },
+  {
+    "revision": "81ec4222fcb93d330cd5",
+    "url": "/crud-react-app/static/js/runtime-main.a5d074de.js"
+  }
+]);
